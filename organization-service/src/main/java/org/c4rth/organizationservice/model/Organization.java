7e4f801343e57475403bd08080f8bd8b4548a065
carth <me@carth.xyz>
2024-08-03 09:07:50 +0200

    openapi generator

--- organization-service/src/main/java/org/c4rth/organizationservice/model/Organization.java
+++ organization-service/src/main/java/org/c4rth/organizationservice/model/Organization.java
@@ -3,12 +3,12 @@ package org.c4rth.organizationservice.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 /**
  * Organization Model
--- site-service/src/main/java/org/c4rth/siteservice/model/Site.java
+++ site-service/src/main/java/org/c4rth/siteservice/model/Site.java
@@ -3,13 +3,13 @@ package org.c4rth.siteservice.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 /**
  * Site Model
